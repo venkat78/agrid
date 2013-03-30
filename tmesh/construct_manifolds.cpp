22fd298645f98487c909eaf4973ede0bb4ad56f8
Bujji <Bujji@localhost>
2013-03-30 14:28:01 -0400

    Testing seed white cell for advancing front.

--- tmesh/construct_manifolds.cpp
+++ tmesh/construct_manifolds.cpp
@@ -115,16 +115,8 @@ namespace tmesh {
 							     INT &numBorderEdges, cMANIFOLD *manifold)
   {
     cHALF_EDGE* facetHeOpp = facetHe->Opp();
-    printf("WTME 118: f %d; HET %d HEH %d; Opp: T %d H %d ",
-    		facet->Index(), facetHe->Tail()->Index(), facetHe->Head()->Index(),
-    		facetHeOpp->Tail()->Index(), facetHeOpp->Head()->Index());
-    if (facetHeOpp->Facet() == NULL)
-      printf("f NULL\n");
-    else
-      printf("f %d\n", facetHeOpp->Head()->Index());
     if(!facetHeOpp->IsBorder()) {
       cFACET* nextFacet = facetHeOpp->Facet();
-      printf("nextFacet %d\n", nextFacet->Index());
 
       if(nextFacet->ManifoldIndex() < 0) {
         nextFacet->ManifoldIndex(manifold->Index());
@@ -137,7 +129,6 @@ namespace tmesh {
         //checking for MOBIUS strip condition.
         BOOL facetShouldFlip = facetsToBeFlipped[facet->Index()];
         BOOL nextFacetShouldFlip = facetsToBeFlipped[nextFacet->Index()];
-        printf("facetShouldFlip %d; nextFacetShouldFlip %d\n", facetShouldFlip, nextFacetShouldFlip);
 
         if((facetShouldFlip && !nextFacetShouldFlip) ||
            (!facetShouldFlip && nextFacetShouldFlip))
@@ -196,7 +187,6 @@ namespace tmesh {
       numFacets++;
 
       BOOL hasNonManifoldEdges = facet->HasNonManifoldEdge();
-      printf("Construct 190: f %d; hasNME %d\n", facet->Index(), hasNonManifoldEdges);
 
       typename cFACET::half_edge_circulator nextHe = facet->HalfEdgesBegin();
       typename cFACET::half_edge_circulator lastHe = facet->HalfEdgesEnd();
