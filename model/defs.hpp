2603d91f068ee40d5453825dabf29312c8fbfbee
Raju Mantena <venkat@caebridge.com>
2013-04-27 10:42:08 -0400

    Definition of group.

---
+++ model/defs.hpp
@@ -0,0 +1,29 @@
+/*
+ * defs.hpp
+ *
+ *  Created on: Apr 27, 2013
+ *      Author: Bujji
+ */
+
+#ifndef MODEL_DEFS_H_
+#define MODEL_DEFS_H_
+
+#include "utils.hpp"
+using namespace utils;
+
+#include "geom.hpp"
+using namespace geom;
+
+#include "tmesh.hpp"
+using namespace tmesh;
+
+#include "mesh_utils.hpp"
+using namespace mesh_utils;
+
+#include "surface_mesh.hpp"
+
+namespace model {
+	typedef INT iINDEX;
+}
+
+#endif /* DEFS_H_ */
---
+++ model/group.hpp
@@ -0,0 +1,105 @@
+/*
+ * collection.hpp
+ *
+ *  Created on: Apr 27, 2013
+ *      Author: Bujji
+ */
+
+#ifndef _MODEL_GROUP_HPP_
+#define _MODEL_GROUP_HPP_
+
+#include "defs.hpp"
+#include <cstdio>
+#include <string>
+#include <vector>
+
+namespace model
+{
+  template<typename MESH_TYPE, typename ATTRIBUTES_TYPE>
+    struct tGROUP
+    {
+      typedef tGROUP<MESH_TYPE, ATTRIBUTES_TYPE> cSELF;
+
+    public:
+
+      iINDEX
+      GetIndex()
+      {
+        return m_index;
+      }
+
+      INT
+      NumChildren()
+      {
+        return m_children.size();
+      }
+
+      cSELF*
+      GetParent()
+      {
+        return m_parent;
+      }
+
+      cSELF*
+      GetChildGroup(const std::string &name);
+
+      VOID
+      SetIndex(iINDEX index)
+      {
+        m_index = index;
+      }
+
+      VOID
+      SetMesh(MESH_TYPE *mesh);
+
+      MESH_TYPE*
+      GetMesh()
+      {
+        return m_mesh;
+      }
+
+      BOOL
+      IsMeshRoot()
+      {
+        return (m_mesh != NULL)
+            && ((m_parent == NULL) || (m_parent->m_mesh == NULL));
+      }
+
+      std::vector<cSELF*>*
+      GetChildren()
+      {
+        return &m_children;
+      }
+
+    public:
+      VOID
+      SetParent(cSELF *parent)
+      {
+        m_parent = parent;
+      }
+
+    public:
+      tGROUP()
+      {
+        m_attributes = NULL;
+        m_mesh = NULL;
+        m_parent = NULL;
+      }
+
+      ~tGROUP()
+      {
+      }
+
+    protected:
+      cSELF* m_parent;
+      MESH_TYPE *m_mesh;
+      std::string m_type;
+      std::string m_name;
+      std::vector<cSELF*> m_children;
+      iINDEX m_index;
+      ATTRIBUTES_TYPE *m_attributes;
+    };
+
+}
+
+#endif /* GROUP_HPP_ */
