400dded1f899439f7e31413afd04015a5c48ba26
Raju Mantena <venkat@caebridge.com>
2013-05-02 13:58:46 -0400

    Code cleanup.

--- io/amoeba_io.cpp
+++ io/amoeba_io.cpp
@@ -9,7 +9,7 @@
 using namespace H5;
 
 #include "amoeba_io.hpp"
-
+#include <iostream>
 namespace io
 {
 	typedef cAMOEBA_MODEL::cGROUP cGROUP;
@@ -26,59 +26,60 @@ namespace io
 				Group childGroup = g.openGroup(objName);
 				cGROUP *cgInModel = model.CreateGroup();
 				cgInModel->SetName(objName);
-				std::cout<<pgInModel->GetName().c_str()<< '/'<<cgInModel->GetName().c_str()<<std::endl;
+				//std::cout<<pgInModel->GetName().c_str()<< '/'<<cgInModel->GetName().c_str()<<std::endl;
 				pgInModel->AddChildGroup(cgInModel);
 				ReadAmoebaGroup(childGroup, cgInModel, model);
+			}
+			else if (objType == H5G_DATASET) {
+				//readDataSet(g.openDataSet(objName), objName, absPath);
+			}
 		}
-		else if (objType == H5G_DATASET) {
-			//readDataSet(g.openDataSet(objName), objName, absPath);
-		}
+
+		return true;
 	}
 
-	return true;
-}
-BOOL ImportAmoebaModel(const std::string &fname, cAMOEBA_MODEL &model) {
-	try {
-		/*
-		 * Turn off the auto-printing when failure occurs so that we can
-		 * handle the errors appropriately
-		 */
-		Exception::dontPrint();
-		std::string GROUP_NAME("sim");
-		/*
-		 * Open the specified file and the specified dataset in the file.
-		 */
-		H5File file(fname, H5F_ACC_RDONLY);
-		cGROUP *simGroup = model.CreateGroup();
-		model.SetRootGroup(simGroup);
-		simGroup->SetName(GROUP_NAME);
-		Group hdfGroup = file.openGroup(GROUP_NAME);
+	BOOL ImportAmoebaModel(const std::string &fname, cAMOEBA_MODEL &model) {
+		try {
+			/*
+			 * Turn off the auto-printing when failure occurs so that we can
+			 * handle the errors appropriately
+			 */
+			Exception::dontPrint();
+			std::string GROUP_NAME("sim");
+			/*
+			 * Open the specified file and the specified dataset in the file.
+			 */
+			H5File file(fname, H5F_ACC_RDONLY);
+			cGROUP *simGroup = model.CreateGroup();
+			model.SetRootGroup(simGroup);
+			simGroup->SetName(GROUP_NAME);
+			Group hdfGroup = file.openGroup(GROUP_NAME);
 
-		ReadAmoebaGroup(hdfGroup, simGroup, model);
-		//readGroup(simGroup, "sim", "");
-		file.close();
+			ReadAmoebaGroup(hdfGroup, simGroup, model);
+			//readGroup(simGroup, "sim", "");
+			file.close();
 
-	}
+		}
 
 // catch failure caused by the DataSet operations
-	catch (DataSetIException error) {
-		//error.printError();
-		return false;
-	}
+		catch (DataSetIException error) {
+			//error.printError();
+			return false;
+		}
 
 // catch failure caused by the DataSpace operations
-	catch (DataSpaceIException error) {
-		//error.printError();
-		return false;
-	}
+		catch (DataSpaceIException error) {
+			//error.printError();
+			return false;
+		}
 
 // catch failure caused by the DataSpace operations
-	catch (DataTypeIException error) {
-		//error.printError();
-		return false;
-	}
+		catch (DataTypeIException error) {
+			//error.printError();
+			return false;
+		}
 
-	return true;
-}
+		return true;
+	}
 }
 
