8ae1844542046d137a0fd5d45c70caf7f7acacfe
Raju Mantena <venkat@caebridge.com>
2013-05-04 16:37:12 -0400

    Now can load Amoeba meshes.

--- io/amoeba_io.cpp
+++ io/amoeba_io.cpp
@@ -8,16 +8,97 @@
 #include "H5Cpp.h"
 using namespace H5;
 
+#include "off.hpp"
 #include "amoeba_io.hpp"
-#include <iostream>
+
 namespace io
 {
 	typedef cAMOEBA_MODEL::cGROUP cGROUP;
+
+	/*
+	 * Find if a group is a mesh.
+	 */
+	BOOL IsAmoebaGroupAMesh(Group &g) {
+		BOOL hasFaceTessConn, hasFaceTessPoints, hasFaceTessSizes;
+		hasFaceTessConn = hasFaceTessPoints = hasFaceTessSizes = false;
+
+		hsize_t numObjs = g.getNumObjs();
+		for (INT i = 0; i < numObjs; i++) {
+			std::string objName = g.getObjnameByIdx(i);
+			if (objName.compare("faceTessellationConnectivity") == 0) {
+				hasFaceTessConn = true;
+			}
+			else if (objName.compare("faceTessellationPoints") == 0) {
+				hasFaceTessPoints = true;
+			}
+			else if (objName.compare("faceTessellationSizes") == 0) {
+				hasFaceTessSizes = true;
+			}
+
+			if (hasFaceTessConn && hasFaceTessPoints && hasFaceTessSizes)
+				return true;
+		}
+
+		return false;
+	}
+
+	VOID ReadAmoebaMesh(Group &g, cGROUP *pgInModel, cAMOEBA_MODEL &model) {
+		REAL coords[250000][3];
+		cAMOEBA_MODEL::cMESH *mesh = model.CreateMesh();
+		mesh->SetName(pgInModel->GetName());
+		pgInModel->SetMesh(mesh);
+
+		/*
+		 * Add vertices to mesh.-
+		 */
+		DataSet pointSet = g.openDataSet("faceTessellationPoints");
+		DataSpace points = pointSet.getSpace();
+		/*
+		 * Get the dimension size of each dimension in the dataspace and
+		 * display them.
+		 */
+		hsize_t vertex_dims[2];
+		int rank = points.getSimpleExtentNdims();
+		INT ndims = points.getSimpleExtentDims(vertex_dims, NULL);
+
+		//Read vertices
+		pointSet.read(coords, PredType::NATIVE_DOUBLE, DataSpace::ALL, points);
+
+		//Add vertices for mesh.
+		for (INT i = 0; i < vertex_dims[0]; i++) {
+			mesh->NewVertex(coords[i][0], coords[i][1], coords[i][2]);
+		}
+
+		DataSet facetSet = g.openDataSet("faceTessellationConnectivity");
+		DataSpace facets = facetSet.getSpace();
+
+		/*
+		 * Get the dimension size of each dimension in the dataspace and
+		 * display them.
+		 */
+		hsize_t facet_dims;
+		ndims = facets.getSimpleExtentDims(&facet_dims, NULL);
+
+		INT facetNodes[500000];
+		facetSet.read(facetNodes, PredType::NATIVE_INT, DataSpace::ALL, facets);
+
+		//Read vertices
+		for (INT i = 0; i < facet_dims; i = i + 3) {
+			mesh->NewTriangle(facetNodes[i], facetNodes[i + 1], facetNodes[i + 2]);
+		}
+
+		ExportToOff<cAMOEBA_MODEL::cMESH>((char*) (pgInModel->GetName().c_str()), *mesh);
+	}
 
 	BOOL ReadAmoebaGroup(Group &g, cGROUP *pgInModel, cAMOEBA_MODEL &model) {
 		/*
 		 * Constructing the tree structure
 		 */
+		if (IsAmoebaGroupAMesh(g)) {
+			ReadAmoebaMesh(g, pgInModel, model);
+			return true;
+		}
+
 		hsize_t numObjs = g.getNumObjs();
 		for (int i = 0; i < numObjs; i++) {
 			H5G_obj_t objType = g.getObjTypeByIdx(i);
@@ -26,7 +107,6 @@ namespace io
 				Group childGroup = g.openGroup(objName);
 				cGROUP *cgInModel = model.CreateGroup();
 				cgInModel->SetName(objName);
-				//std::cout<<pgInModel->GetName().c_str()<< '/'<<cgInModel->GetName().c_str()<<std::endl;
 				pgInModel->AddChildGroup(cgInModel);
 				ReadAmoebaGroup(childGroup, cgInModel, model);
 			}
